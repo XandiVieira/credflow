1bd599509535faf3c874c6add23e0b0b7f16a73c
Polyana Fucilini <poly.fucilini.s@gmail.com>
2025-11-07 08:46:11 -0300

    feat(transactions): Mudança na Nomenclatura dos Endpoints de Select

---
+++ src/main/java/com/relyon/credflow/model/credit_card/CreditCardSelectDTO.java
@@ -0,0 +1,13 @@
+package com.relyon.credflow.model.credit_card;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreditCardSelectDTO {
+    private Long id;
+    private String description;
+}
